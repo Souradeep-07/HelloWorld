c932b5456888ebdee9aca093a6d8c9c0bc24b3cf
Souradeep-07 <73077821+Souradeep-07@users.noreply.github.com>
2022-07-13 10:51:15 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+package pck;
+import java.util.*;
+import java.sql.SQLOutput;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
